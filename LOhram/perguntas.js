b745ad08dc01fced5cb4ecea078f73834621f642
Lohram <165791955+Lohram@users.noreply.github.com>
2024-11-12 12:40:37 -0300

    Add files via upload

---
+++ LOhram/perguntas.js
@@ -0,0 +1,31 @@
+criaCartao(
+    'gays',
+    'O que quer dizer LGBT?',
+    'lésbicas, gays, bissexuais, transgêneros e queer'
+)
+
+criaCartao(
+    'gays',
+    'O que é orientação sexual?',
+    'está ligada ao que a pessoa busca em relacionamento afetivos e sexuais'
+)
+
+criaCartao(
+    'gays',
+    'O que é identidade de gênero?',
+    'experiência interna e individual relacionada ao gênero com o qual a pessoa se identifica'
+)
+
+criaCartao(
+
+    'gays',
+    'O que é identidade de gênero',
+    'Experiência interna e individual relacionada ao gênero com o qual a pessoa se identifica'
+)
+
+criaCartao(
+
+    'gays',
+    'O que é intersexo?',
+    'Tem caracteristicas sexuais congênitas que não se enquadram nas normas médicas'
+)
